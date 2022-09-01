403bad89398bbefc4a42a4c1eecc9d8baa6e69cb
Alfonsinfo <itsmealfons@gmail.com>
2022-09-01 07:20:39 +0700

    activity main ( kt,xml) -> activity login. login form,belum dicode

--- app/src/main/java/com/kelompok11/pbp_elearning/loginActivity.kt
+++ app/src/main/java/com/kelompok11/pbp_elearning/loginActivity.kt
@@ -1,11 +1,11 @@
-package com.kelompok11.pbp_elearning
+    package com.kelompok11.pbp_elearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
